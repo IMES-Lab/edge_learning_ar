6612d7ed02c20c701c7cb93cbe8de1145517c6f2
Ji Hu <32670168+Ji-Hu@users.noreply.github.com>
2018-11-07 18:12:48 +0900

    Update CNNmodel.py

--- CNNmodel.py
+++ CNNmodel.py
@@ -1,5 +1,6 @@
-# Lab 11 MNIST and Deep learning CNN
+# Lab 10 MNIST and Deep learning
 import tensorflow as tf
+import random
 # import matplotlib.pyplot as plt
 
 from tensorflow.examples.tutorials.mnist import input_data
@@ -10,130 +11,50 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 # Check out https://www.tensorflow.org/get_started/mnist/beginners for
 # more information about the mnist dataset
 
-# hyper parameters
+# parameters
 learning_rate = 0.001
 training_epochs = 15
 batch_size = 100
 
+# input place holders
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32, [None, 10])
 
-class Model:
+# weights & bias for nn layers
+# http://stackoverflow.com/questions/33640581/how-to-do-xavier-initialization-on-tensorflow
+W1 = tf.get_variable("W1", shape=[784, 512],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([512]))
+L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
 
-    def __init__(self, sess, name):
-        self.sess = sess
-        self.name = name
-        self._build_net()
+W2 = tf.get_variable("W2", shape=[512, 512],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([512]))
+L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
 
-    def _build_net(self):
-        with tf.variable_scope(self.name):
-            # dropout (keep_prob) rate  0.7~0.5 on training, but should be 1
-            # for testing
-            self.keep_prob = tf.placeholder(tf.float32)
+W3 = tf.get_variable("W3", shape=[512, 512],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([512]))
+L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
 
-            # input place holders
-            self.X = tf.placeholder(tf.float32, [None, 784])
-            # img 28x28x1 (black/white)
-            X_img = tf.reshape(self.X, [-1, 28, 28, 1])
-            self.Y = tf.placeholder(tf.float32, [None, 10])
+W4 = tf.get_variable("W4", shape=[512, 512],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([512]))
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
 
-            # L1 ImgIn shape=(?, 28, 28, 1)
-            W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
-            #    Conv     -> (?, 28, 28, 32)
-            #    Pool     -> (?, 14, 14, 32)
-            L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
-            L1 = tf.nn.relu(L1)
-            L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1],
-                                strides=[1, 2, 2, 1], padding='SAME')
-            L1 = tf.nn.dropout(L1, keep_prob=self.keep_prob)
-            '''
-            Tensor("Conv2D:0", shape=(?, 28, 28, 32), dtype=float32)
-            Tensor("Relu:0", shape=(?, 28, 28, 32), dtype=float32)
-            Tensor("MaxPool:0", shape=(?, 14, 14, 32), dtype=float32)
-            Tensor("dropout/mul:0", shape=(?, 14, 14, 32), dtype=float32)
-            '''
+W5 = tf.get_variable("W5", shape=[512, 10],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.matmul(L4, W5) + b5
 
-            # L2 ImgIn shape=(?, 14, 14, 32)
-            W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
-            #    Conv      ->(?, 14, 14, 64)
-            #    Pool      ->(?, 7, 7, 64)
-            L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
-            L2 = tf.nn.relu(L2)
-            L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],
-                                strides=[1, 2, 2, 1], padding='SAME')
-            L2 = tf.nn.dropout(L2, keep_prob=self.keep_prob)
-            '''
-            Tensor("Conv2D_1:0", shape=(?, 14, 14, 64), dtype=float32)
-            Tensor("Relu_1:0", shape=(?, 14, 14, 64), dtype=float32)
-            Tensor("MaxPool_1:0", shape=(?, 7, 7, 64), dtype=float32)
-            Tensor("dropout_1/mul:0", shape=(?, 7, 7, 64), dtype=float32)
-            '''
-
-            # L3 ImgIn shape=(?, 7, 7, 64)
-            W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
-            #    Conv      ->(?, 7, 7, 128)
-            #    Pool      ->(?, 4, 4, 128)
-            #    Reshape   ->(?, 4 * 4 * 128) # Flatten them for FC
-            L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
-            L3 = tf.nn.relu(L3)
-            L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1], strides=[
-                                1, 2, 2, 1], padding='SAME')
-            L3 = tf.nn.dropout(L3, keep_prob=self.keep_prob)
-
-            L3_flat = tf.reshape(L3, [-1, 128 * 4 * 4])
-            '''
-            Tensor("Conv2D_2:0", shape=(?, 7, 7, 128), dtype=float32)
-            Tensor("Relu_2:0", shape=(?, 7, 7, 128), dtype=float32)
-            Tensor("MaxPool_2:0", shape=(?, 4, 4, 128), dtype=float32)
-            Tensor("dropout_2/mul:0", shape=(?, 4, 4, 128), dtype=float32)
-            Tensor("Reshape_1:0", shape=(?, 2048), dtype=float32)
-            '''
-
-            # L4 FC 4x4x128 inputs -> 625 outputs
-            W4 = tf.get_variable("W4", shape=[128 * 4 * 4, 625],
-                                 initializer=tf.contrib.layers.xavier_initializer())
-            b4 = tf.Variable(tf.random_normal([625]))
-            L4 = tf.nn.relu(tf.matmul(L3_flat, W4) + b4)
-            L4 = tf.nn.dropout(L4, keep_prob=self.keep_prob)
-            '''
-            Tensor("Relu_3:0", shape=(?, 625), dtype=float32)
-            Tensor("dropout_3/mul:0", shape=(?, 625), dtype=float32)
-            '''
-
-            # L5 Final FC 625 inputs -> 10 outputs
-            W5 = tf.get_variable("W5", shape=[625, 10],
-                                 initializer=tf.contrib.layers.xavier_initializer())
-            b5 = tf.Variable(tf.random_normal([10]))
-            self.logits = tf.matmul(L4, W5) + b5
-            '''
-            Tensor("add_1:0", shape=(?, 10), dtype=float32)
-            '''
-
-        # define cost/loss & optimizer
-        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            logits=self.logits, labels=self.Y))
-        self.optimizer = tf.train.AdamOptimizer(
-            learning_rate=learning_rate).minimize(self.cost)
-
-        correct_prediction = tf.equal(
-            tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    def predict(self, x_test, keep_prop=1.0):
-        return self.sess.run(self.logits, feed_dict={self.X: x_test, self.keep_prob: keep_prop})
-
-    def get_accuracy(self, x_test, y_test, keep_prop=1.0):
-        return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.keep_prob: keep_prop})
-
-    def train(self, x_data, y_data, keep_prop=0.7):
-        return self.sess.run([self.cost, self.optimizer], feed_dict={
-            self.X: x_data, self.Y: y_data, self.keep_prob: keep_prop})
+# define cost/loss & optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # initialize
 sess = tf.Session()
-m1 = Model(sess, "m1")
-
 sess.run(tf.global_variables_initializer())
-
-print('Learning Started!')
 
 # train my model
 for epoch in range(training_epochs):
@@ -142,7 +63,8 @@ for epoch in range(training_epochs):
 
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        c, _ = m1.train(batch_xs, batch_ys)
+        feed_dict = {X: batch_xs, Y: batch_ys}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
         avg_cost += c / total_batch
 
     print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
@@ -150,4 +72,17 @@ for epoch in range(training_epochs):
 print('Learning Finished!')
 
 # Test model and check accuracy
-print('Accuracy:', m1.get_accuracy(mnist.test.images, mnist.test.labels))
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: mnist.test.images, Y: mnist.test.labels}))
+
+# Get one and predict
+r = random.randint(0, mnist.test.num_examples - 1)
+print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+print("Prediction: ", sess.run(
+    tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
+
+# plt.imshow(mnist.test.images[r:r + 1].
+#           reshape(28, 28), cmap='Greys', interpolation='nearest')
+# plt.show()
